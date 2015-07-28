50e3629ee5eb7db856ed35225a0f3e8bbf6ac0d0
rewfe <qrewfe@yandex.ru>
2015-07-28 03:00:03 +0300

    Keyframe Animations

--- View Animations Demo/ExampleIVViewController.swift
+++ View Animations Demo/ExampleIVViewController.swift
@@ -39,4 +39,30 @@ class ExampleIVViewController: UITableViewController {
         return cell
     }
     
+    override func viewWillAppear(animated: Bool) {
+        animateTable()
+    }
+    
+    func animateTable() {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells()
+        let tableHeight: CGFloat = tableView.bounds.size.height
+        
+        for i in cells {
+            let cell: UITableViewCell = i as! UITableViewCell
+            cell.transform = CGAffineTransformMakeTranslation(0, tableHeight)
+        }
+        
+        var index = 0
+        
+        for a in cells {
+            let cell: UITableViewCell = a as! UITableViewCell
+            UIView.animateWithDuration(1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: nil, animations: {
+                cell.transform = CGAffineTransformMakeTranslation(0, 0);
+                }, completion: nil)
+            
+            index += 1
+        }
+    }
 }
